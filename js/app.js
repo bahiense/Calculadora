1a32aaca0d86414347d8cbac7275ec56742c3aa8
Rodrigo Bahiense <rodrigobahiense@gmail.com>
2020-02-08 13:00:45 -0300

    inicio do javascript

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+document
+  .querySelector(".calculator")
+  .addEventListener("click", function(event) {
+    buttonClick(event.target.innerText);
+  });
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+}
+
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+    console.log(buffer);
+  } else {
+    buffer += value;
+  }
+  console.log(buffer);
+}
