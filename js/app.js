12ef9765ad496ce4bf26d55e29d24e4e828020e1
Rodrigo Bahiense <rodrigobahiense@gmail.com>
2020-02-10 16:09:23 -0300

    js adicionado. Resultado final

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
+const screen = document.querySelector(".result");
 
 document
   .querySelector(".calculator")
@@ -14,13 +15,69 @@ function buttonClick(value) {
   } else {
     handleNumber(value);
   }
+  rerender();
 }
 
 function handleNumber(value) {
   if (buffer === "0") {
-    console.log(buffer);
+    buffer = value;
   } else {
     buffer += value;
   }
-  console.log(buffer);
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+}
+
+function handleMath(value) {
+  const intBuffer = parseInt(buffer);
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = value;
+  buffer = "0";
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "-") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "x") {
+    runningTotal *= intBuffer;
+  } else {
+    runningTotal /= intBuffer;
+  }
+}
+
+function rerender() {
+  screen.innerText = buffer;
 }
